9fd22312619d020439b2f7e74e48fa6676e6a0a0
Perry Fustero <pjfust@gmail.com>
2018-03-25 21:11:29 -0400

    create filter for bedroom min/max

--- front-end-interview-test/src/CHANGEME.js
+++ front-end-interview-test/src/CHANGEME.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RemineTable from './components/Table/RemineTable/RemineTable';
-import Api from './API'
+import Api from './API';
+import './CHANGEME.css';
 
 class Test extends Component {
     constructor() {
@@ -12,17 +13,16 @@ class Test extends Component {
         }
     }
 
-    
     componentDidMount() {
         Api.getBuildingTypes()
             .catch(err => console.log(err))
             .then(res => {
-                console.log(res)
                 this.setState({buildingTypes: res.data})
             })
         Api.getLocations()
             .catch(err => console.log(err))
             .then(res => {
+                console.log(res.data)
                 this.setState({
                     data: res.data,
                     filteredData: res.data
@@ -30,24 +30,65 @@ class Test extends Component {
             })
     }
 
-    handleFilter(type) {
-        console.log(type)
-        let filterResult = this.state.data.filter(data => {
-            return data.buildingType.name === type
+    handleFilter(e) {
+        let bedMin = e.target.bedroomsMin.value
+        let bedMax = e.target.bedroomsMax.value
+        let bathMin = e.target.bathroomsMin.value
+        let bathMax = e.target.bathroomsMax.value
+        this.setState({
+            filteredData: this.state.filteredData.filter((data) => {
+                return (data.beds >= bedMin && data.beds <= bedMax)
+            })
         })
-        this.setState({filteredData: filterResult})
     }
 
     render() {
+        console.log(this.state.filteredData)
         return (
             <div className="testContainer">
-                {this.state.buildingTypes.map(type => (
-                    <button 
-                        key={type.id} 
-                        onClick={e => this.handleFilter(e.target.innerHTML)}>
-                            {type.name}
-                    </button>
-                ))}
+                {
+                //     this.state.buildingTypes.map(type => (
+                //     <button 
+                //         key={type.id} 
+                //         onClick={e => this.handleFilter(e.target.innerHTML)}>
+                //             {type.name}
+                //     </button>
+                // ))
+                }
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    this.handleFilter(e)
+                }}>
+                    <input 
+                        type="number" 
+                        name="bedroomsMin"
+                        placeholder="Minimum number of Bedrooms"
+                        className="filter-input"
+                    />
+                    <input 
+                        type="number" 
+                        name="bedroomsMax"
+                        placeholder="Max number of Bedrooms"
+                        className="filter-input"
+                    />
+                    <input 
+                        type="number" 
+                        name="bathroomsMin"
+                        placeholder="Minimum number of Bathrooms"
+                        className="filter-input"
+                    />
+                    <input 
+                        type="number" 
+                        name="bathroomsMax"
+                        placeholder="Maximum number of Bathrooms"
+                        className="filter-input"
+                    />
+                    <input 
+                        type="submit"
+                        className="filter-input filter-button"
+                        value="Filter Results"
+                    />
+                </form>
                 <div className="filterContainer">
                 </div>
                 <RemineTable properties={this.state.filteredData} />
